ea7b29a22de2ef51da26a0745b5b6c99bb9dbfa2
loss <kalyansky@gmail.com>
2013-10-29 09:05:27 +0300

    use plist module instead of a bicycle one

--- bin/build-app-settings.js
+++ bin/build-app-settings.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var builder = require('xmlbuilder');
-var doc = builder.create();
+var plist = require('plist');
 
 function ucfirst(s) {
 	return s.charAt(0).toUpperCase() + s.substring(1);
@@ -14,15 +13,32 @@ var ConfigMap = function(config) {
 		switch (config.type) {
 			case 'combo':
 				config.type = 'PSMultiValueSpecifier';
-				config.DefaultValue = config.default;
+
+				config.DefaultValue = config['default'];
 				delete config['default'];
+
 				config.Key = config.name;
 				delete config['name'];
+
+				config.titles = [];
+				config.values = [];
+				config.items.forEach(function(a) {
+					config.values.push(a.id || a.value);
+					config.titles.push(a.name || a.title);
+				});
+				delete config.items;
 			break;
 			case 'group':
 				config.type = 'PSGroupSpecifier';
 			break;
 	}
+
+	Object.keys(config).forEach(function(k) {
+		var uc = ucfirst(k);
+		config[uc] = config[k];
+		if (uc != k)
+			delete config[k];
+	})
 
 	return config;
 }
@@ -33,45 +49,43 @@ fs.readFile('app-settings.json', function(err, data) {
 		throw err;
 
 	data = JSON.parse(data);
-	var doc = builder.create();
-	var list = doc.begin('plist').att('version', '1.0')
-		.ele('dict')
-			.ele('key', 'PreferenceSpecifiers')
-			.up()
-		.ele('array');
-
-
-	function addSetting(config) {
-		var type = config.type || '';
-		config = ConfigMap(config);
-		var dict = list.ele('dict');
-
-		for (key in config) {
-			if (key == "items")
-				continue;
-			dict.ele('key', ucfirst(key));
-			dict.ele('string', config[key]);
-		}
-
-		var items = typeof config.items == "object" && config.items.length ? config.items : [];
-		if (type == "group") {
-			items.forEach(function(sub) {
-				addSetting(sub);
+	var items = [];
+	while (data.length) {
+		var src = data.shift();
+		if (src.type == 'group') {
+			src.items.forEach(function(s) {
+				data.unshift(s);
 			});
-		} else if (type == "combo") {
-			var v = dict.ele('key', 'Values').up().ele('array'),
-			    t = dict.ele('key', 'Titles').up().ele('array');
-			items.forEach(function(sub) {
-				v.ele('string', sub.value);
-				t.ele('string', sub.title || sub.name || sub.value);
-			});
+			delete src['items'];
 		}
+		items.push(ConfigMap(src));
 	}
 
-	data.forEach(function(el) {
-		addSetting(el);
+	var xml = plist.build({ PreferenceSpecifiers: items });
+	fs.exists('platforms/ios', function(exists) {
+		if (!exists)
+			throw 'platform ios not found';
+
+		fs.mkdir('platforms/ios/Settings.bundle', function(e) {
+			if (e && e.code != 'EEXIST')
+				throw e;
+
+			// Write settings plist
+			fs.writeFile('platforms/ios/Settings.bundle/Root.plist', xml, function(err) {
+				if (err)
+					throw err;
+				console.log('plist was successfully generated');
+			});
+
+			// Write localization resource file
+			fs.mkdir('platforms/ios/Settings.bundle/en.lproj', function(e) {
+				if (e && e.code != 'EEXIST')
+					throw e;
+				fs.writeFile('platforms/ios/Settings.bundle/en.lproj/Root.strings', '/* */', function(err) {
+					if (err)
+						throw err;
+				});
+			});
+		});
 	});
-
-
-	console.log(doc.toString({ pretty: true }));
 });
