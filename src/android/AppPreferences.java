8c5003e288af7aeffd8780d0732c6a6f80151137
apla <dot.and.thing@gmail.com>
2014-02-23 15:24:44 +0400

    rewritten plugins to use background thread

--- src/android/AppPreferences.java
+++ src/android/AppPreferences.java
@@ -3,9 +3,6 @@ package me.apla.cordova;
 //import java.util.Iterator;
 //import java.util.Map;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -30,31 +27,31 @@ import android.util.Log;
 
 <?xml version="1.0" encoding="utf-8"?>
 <PreferenceScreen xmlns:android="http://schemas.android.com/apk/res/android">
-    <PreferenceCategory android:title="Main">
-        <CheckBoxPreference android:title="Enable Preferences"
-            android:key="EnablePreferences" android:summary="Check to enable Other Preferences" />
-    </PreferenceCategory>
-    <PreferenceCategory android:title="Other Prefernces">
-        <ListPreference android:title="List Preference"
-            android:key="DayOfWeek" android:dependency="EnablePreferences"
-            android:summary="Selec Day of the Week" android:entries="@array/daysOfWeek"
-            android:entryValues="@array/daysOfWeekValues" />
-        <EditTextPreference android:title="Edit Text Preference"
-            android:key="Name" android:dependency="EnablePreferences"
-            android:summary="Enter Your Name" android:dialogTitle="Enter Your Name"
-            android:defaultValue="Android Partaker"/>
-        <RingtonePreference android:title="Ringtone Preference"
-            android:key="Ringtone" android:dependency="EnablePreferences"
-            android:summary="Select Ringtone" android:ringtoneType="all" />
-    </PreferenceCategory>
- 
-    <PreferenceCategory android:title="Advance Preference">
-        <PreferenceScreen android:title="Advance Preference">
- 
-            <EditTextPreference android:title="Enter Text"
-                android:key="Text" />
-        </PreferenceScreen>
-    </PreferenceCategory>
+	<PreferenceCategory android:title="Main">
+		<CheckBoxPreference android:title="Enable Preferences"
+			android:key="EnablePreferences" android:summary="Check to enable Other Preferences" />
+	</PreferenceCategory>
+	<PreferenceCategory android:title="Other Prefernces">
+		<ListPreference android:title="List Preference"
+			android:key="DayOfWeek" android:dependency="EnablePreferences"
+			android:summary="Selec Day of the Week" android:entries="@array/daysOfWeek"
+			android:entryValues="@array/daysOfWeekValues" />
+		<EditTextPreference android:title="Edit Text Preference"
+			android:key="Name" android:dependency="EnablePreferences"
+			android:summary="Enter Your Name" android:dialogTitle="Enter Your Name"
+			android:defaultValue="Android Partaker"/>
+		<RingtonePreference android:title="Ringtone Preference"
+			android:key="Ringtone" android:dependency="EnablePreferences"
+			android:summary="Select Ringtone" android:ringtoneType="all" />
+	</PreferenceCategory>
+
+	<PreferenceCategory android:title="Advance Preference">
+		<PreferenceScreen android:title="Advance Preference">
+
+			<EditTextPreference android:title="Enter Text"
+				android:key="Text" />
+		</PreferenceScreen>
+	</PreferenceCategory>
 </PreferenceScreen>
 
 */
@@ -64,95 +61,32 @@ public class AppPreferences extends CordovaPlugin {
 //    private static final String LOG_TAG = "AppPreferences";
 //    private static final int NO_PROPERTY = 0;
 //    private static final int NO_PREFERENCE_ACTIVITY = 1;
-    private static final int COMMIT_FAILED = 2;
+	private static final int COMMIT_FAILED = 2;
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+	@Override
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 //        String result = "";
 
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
 
-        if (action.equals("fetch")) {
-                JSONObject options = args.getJSONObject (0);
-                String key  = options.getString("key");
-                String dict = options.optString("dict");
-                if (!"".equals(dict))
-                    key = dict + '.' + key;
-                String returnVal = null;
-            if (sharedPrefs.contains(key)) {
-                Object obj = sharedPrefs.getAll().get(key);
-                String objClass = obj.getClass().getName();
-                if (objClass.equals("java.lang.Integer")) {
-                    returnVal = obj.toString();
-                } else if (objClass.equals("java.lang.Float") || objClass.equals("java.lang.Double")) {
-                    returnVal = obj.toString();
-                } else if (objClass.equals("java.lang.Boolean")) {
-                    returnVal = (Boolean)obj ? "true" : "false";
-                } else if (objClass.equals("java.lang.String")) {
-                    if (sharedPrefs.contains("_" + key + "_type")) {
-                        // here we have json encoded string
-                        returnVal = (String)obj;
-                    } else {
-                            String fakeArray = new JSONStringer().array().value((String)obj).endArray().toString();
-                            returnVal = fakeArray.substring(1, fakeArray.length()-1);
-                            // returnVal = new JSONStringer().value((String)obj).toString();
-                    }
-                    
-                } else {
-                    Log.d("", "unhandled type: " + objClass);
-                }
-                // JSONObject jsonValue = new JSONObject((Map) obj);
-                callbackContext.success(returnVal);
-            } else {
-//              Log.d("", "no value");
-                    callbackContext.error(0);
-//                callbackContext.sendPluginResult(new PluginResult ());
-            }
-            return true;
-        } else if (action.equals("store")) {
-            JSONObject options = args.getJSONObject (0);
-            String key    = options.getString("key");
-            String value  = options.getString("value");
-            String dict   = options.optString("dict");
-            String type   = options.optString("type");
-            if (!"".equals(dict))
-                key = dict + '.' + key;
-            Editor editor = sharedPrefs.edit();
-            // editor.putString(key, value);
-            
-                JSONTokener jt = new JSONTokener(value);
-                Object nv = jt.nextValue();
-                String className = nv.getClass().getName();
-            
-//              Log.d("", "value is: " + nv.toString() + " js type is: " + type + " " + args.toString());
-            if (type != null) {
-                if (sharedPrefs.contains("_" + key + "_type")) {
-                    editor.remove("_" + key + "_type");
-                }
-                if (type.equals("string") ) {
-                    editor.putString (key, (String)nv);
-                } else if (type.equals("number")) {
-                        if (className.equals("java.lang.Double")) {
-                            editor.putFloat(key, ((Double) nv).floatValue());
-                        } else if (className.equals("java.lang.Integer")) {
-                            editor.putInt(key, (Integer) nv);
-                        } 
-                } else if (type.equals("boolean")) {
-                    editor.putBoolean (key, (Boolean)nv);
-                } else {
-                    editor.putString(key, value);
-                    editor.putString ("_" + key + "_type", "json");
-//                    Log.d("", "complex thing stored");
-                }
-                        
-            }
-        
-            if (editor.commit()) {
-                callbackContext.success();
-            } else {
-                callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
-            } 
-            return true;
+		if (action.equals("fetch")) {
+			JSONObject options = args.getJSONObject (0);
+			String key  = options.getString("key");
+			String dict = options.optString("dict");
+			if (!"".equals(dict))
+				key = dict + '.' + key;
+			Log.d ("", "key is " + key);
+			return this.fetchValueByKey(key, callbackContext);
+		} else if (action.equals("store")) {
+			JSONObject options = args.getJSONObject (0);
+			String key    = options.getString("key");
+			String value  = options.getString("value");
+			String dict   = options.optString("dict");
+			String type   = options.optString("type");
+			if (!"".equals(dict))
+				key = dict + '.' + key;
+			Log.d ("", "key is " + key);
+			return this.storeValueByKey(key, type, value, callbackContext);
 //            } else if (action.equals("load")) {
 //                JSONObject obj = new JSONObject();
 //                Map prefs = sharedPrefs.getAll();
@@ -171,48 +105,178 @@ public class AppPreferences extends CordovaPlugin {
 //                } catch (ActivityNotFoundException e) {
 //                    callbackContext.sendPluginResult(createErrorObj(NO_PREFERENCE_ACTIVITY, "No preferences activity called " + activityName));
 //                }
-        } else if (action.equals("clearAll")) {
-            Editor editor = sharedPrefs.edit();
-            editor.clear();
-            editor.commit();
-            if (editor.commit()) {
-                callbackContext.success();
-            } else {
-                callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
-            } 
-            return true;
-        } else if (action.equals("remove")) {
-            JSONObject options = args.getJSONObject (0);
-            String key  = options.getString("key");
-            String dict = options.getString("dict");
-            if (!"".equals(dict))
-                    key = dict + '.' + key;
-            if (sharedPrefs.contains(key)) {
-                Editor editor = sharedPrefs.edit();
-                editor.remove(key);
-                    if (sharedPrefs.contains("_" + key + "_type")) {
-                        editor.remove("_" + key + "_type");
-                    }
+		} else if (action.equals("removeAll")) {
+			cordova.getThreadPool().execute(new Runnable() {public void run() {
 
-                if (editor.commit()) {
-                    callbackContext.success();
-                } else {
-                    callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
-                } 
-            } else {
-                callbackContext.sendPluginResult(new PluginResult (PluginResult.Status.NO_RESULT));
-            }
-            return true;
-        }
-        // callbackContext.sendPluginResult(new PluginResult (PluginResult.Status.JSON_EXCEPTION));
-        return false;
-    }
+			SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
 
-    private JSONObject createErrorObj(int code, String message) throws JSONException {
-        JSONObject errorObj = new JSONObject();
-        errorObj.put("code", code);
-        errorObj.put("message", message);
-        return errorObj;
-    }
+			Editor editor = sharedPrefs.edit();
+			editor.clear();
+			editor.commit();
+			if (editor.commit()) {
+				callbackContext.success();
+			} else {
+				try {
+					callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			}});
+			return true;
+		} else if (action.equals("remove")) {
+			JSONObject options = args.getJSONObject (0);
+			String key  = options.getString("key");
+			String dict = options.getString("dict");
+			if (!"".equals(dict))
+					key = dict + '.' + key;
+			removeValueByKey(key, callbackContext);
+		}
+		// callbackContext.sendPluginResult(new PluginResult (PluginResult.Status.JSON_EXCEPTION));
+		return false;
+	}
+
+	private boolean fetchValueByKey(final String key, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {public void run() {
+
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
+		String returnVal = null;
+		if (sharedPrefs.contains(key)) {
+			Object obj = sharedPrefs.getAll().get(key);
+			String objClass = obj.getClass().getName();
+			if (objClass.equals("java.lang.Integer")) {
+				returnVal = obj.toString();
+			} else if (objClass.equals("java.lang.Float") || objClass.equals("java.lang.Double")) {
+				returnVal = obj.toString();
+			} else if (objClass.equals("java.lang.Boolean")) {
+				returnVal = (Boolean)obj ? "true" : "false";
+			} else if (objClass.equals("java.lang.String")) {
+				if (sharedPrefs.contains("_" + key + "_type")) {
+					// here we have json encoded string
+					returnVal = (String)obj;
+				} else {
+					String fakeArray = null;
+					try {
+						fakeArray = new JSONStringer().array().value((String)obj).endArray().toString();
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						callbackContext.error(0);
+						return;
+					}
+					returnVal = fakeArray.substring(1, fakeArray.length()-1);
+					// returnVal = new JSONStringer().value((String)obj).toString();
+				}
+
+			} else {
+				Log.d("", "unhandled type: " + objClass);
+			}
+			// JSONObject jsonValue = new JSONObject((Map) obj);
+			callbackContext.success(returnVal);
+		} else {
+//          Log.d("", "no value");
+				callbackContext.error(0);
+//            callbackContext.sendPluginResult(new PluginResult ());
+		}
+
+		}});
+
+		return true;
+	}
+
+	private boolean removeValueByKey(final String key, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() { public void run() {
+
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
+		if (sharedPrefs.contains(key)) {
+			Editor editor = sharedPrefs.edit();
+			editor.remove(key);
+			if (sharedPrefs.contains("_" + key + "_type")) {
+				editor.remove("_" + key + "_type");
+			}
+
+			if (editor.commit()) {
+				callbackContext.success();
+			} else {
+				try {
+					callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} else {
+			callbackContext.sendPluginResult(new PluginResult (PluginResult.Status.NO_RESULT));
+		}
+
+		}});
+
+		return true;
+	}
+
+	private boolean storeValueByKey(final String key, final String type, final String value, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {public void run() {
+
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
+
+		Editor editor = sharedPrefs.edit();
+		// editor.putString(key, value);
+
+		JSONTokener jt = new JSONTokener(value);
+		Object nv = null;
+		try {
+			nv = jt.nextValue();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String className = nv.getClass().getName();
+
+//      Log.d("", "value is: " + nv.toString() + " js type is: " + type + " " + args.toString());
+		if (type != null) {
+			if (sharedPrefs.contains("_" + key + "_type")) {
+				editor.remove("_" + key + "_type");
+			}
+			if (type.equals("string") ) {
+				editor.putString (key, (String)nv);
+			} else if (type.equals("number")) {
+				if (className.equals("java.lang.Double")) {
+					editor.putFloat(key, ((Double) nv).floatValue());
+				} else if (className.equals("java.lang.Integer")) {
+					editor.putInt(key, (Integer) nv);
+				}
+			} else if (type.equals("boolean")) {
+				editor.putBoolean (key, (Boolean)nv);
+			} else {
+				editor.putString(key, value);
+				editor.putString ("_" + key + "_type", "json");
+//                Log.d("", "complex thing stored");
+			}
+
+		}
+
+		if (editor.commit()) {
+			callbackContext.success();
+		} else {
+			try {
+				callbackContext.error(createErrorObj(COMMIT_FAILED, "Cannot commit change"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		}});
+
+		return true;
+	}
+
+	private JSONObject createErrorObj(int code, String message) throws JSONException {
+		JSONObject errorObj = new JSONObject();
+		errorObj.put("code", code);
+		errorObj.put("message", message);
+		return errorObj;
+	}
 
 }
--- src/ios/AppPreferences.m
+++ src/ios/AppPreferences.m
@@ -1,6 +1,6 @@
 //
 //  AppPreferences.m
-//  
+//
 //
 //  Created by Tue Topholm on 31/01/11.
 //  Copyright 2011 Sugee. All rights reserved.
@@ -15,8 +15,8 @@
 
 - (void)fetch:(CDVInvokedUrlCommand*)command
 {
-	
-	CDVPluginResult* result = nil;
+
+	__block CDVPluginResult* result = nil;
 
 	NSDictionary* options = [[command arguments] objectAtIndex:0];
 
@@ -28,14 +28,15 @@
 
 	NSString *settingsDict = [options objectForKey:@"dict"];
 	NSString *settingsName = [options objectForKey:@"key"];
-	
+
+	[self.commandDelegate runInBackground:^{
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	
+
 	id target = defaults;
-	
+
 	// NSMutableDictionary *mutable = [[dict mutableCopy] autorelease];
 	// NSDictionary *dict = [[mutable copy] autorelease];
-	
+
 	@try {
 
 		if (settingsDict) {
@@ -44,44 +45,47 @@
 
 		NSString *returnVar;
 		id settingsValue = [target objectForKey:settingsName];
-		
+
 		if (settingsValue != nil) {
 			if ([settingsValue isKindOfClass:[NSString class]]) {
 				returnVar = [NSString stringWithFormat:@"\"%@\"", (NSString*)settingsValue];
 			} else if ([settingsValue isKindOfClass:[NSNumber class]]) {
 				if ((NSNumber*)settingsValue == (void*)kCFBooleanFalse || (NSNumber*)settingsValue == (void*)kCFBooleanTrue) {
 //					const char * x = [(NSNumber*)settingsValue objCType];
+//					NSLog(@"boolean %@", [(NSNumber*)settingsValue boolValue] == NO ? @"false" : @"true");
 					returnVar = [NSString stringWithFormat:@"%@", [(NSNumber*)settingsValue boolValue] == YES ? @"true": @"false"];
 				} else {
 					// TODO: int, float
+//					NSLog(@"number");
 					returnVar = [NSString stringWithFormat:@"%@", (NSNumber*)settingsValue];
 				}
-				
+
 			} else if ([settingsValue isKindOfClass:[NSData class]]) { // NSData
 				returnVar = [[NSString alloc] initWithData:(NSData*)settingsValue encoding:NSUTF8StringEncoding];
 			}
 		} else {
 			returnVar = [self getSettingFromBundle:settingsName]; //Parsing Root.plist
-			
+
 //			if (returnVar == nil)
 //				@throw [NSException exceptionWithName:nil reason:@"Key not found" userInfo:nil];;
 		}
-		
+
 		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:returnVar];
 
 	} @catch (NSException * e) {
 
 		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT messageAsString:[e reason]];
-	
+
 	} @finally {
-	
+
 		[self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 	}
+	}];
 }
 
 - (void)store:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* result;
+    __block CDVPluginResult* result;
 
 	NSDictionary* options = [[command arguments] objectAtIndex:0];
 
@@ -96,13 +100,16 @@
     NSString *settingsValue = [options objectForKey:@"value"];
 	NSString *settingsType  = [options objectForKey:@"type"];
 
+//	NSLog(@"%@ = %@ (%@)", settingsName, settingsValue, settingsType);
+
+	[self.commandDelegate runInBackground:^{
 	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	
+
 	id target = defaults;
 
 	// NSMutableDictionary *mutable = [[dict mutableCopy] autorelease];
 	// NSDictionary *dict = [[mutable copy] autorelease];
-	
+
 	if (settingsDict) {
 		target = [[defaults dictionaryForKey:settingsDict] mutableCopy];
 		if (!target) {
@@ -117,13 +124,13 @@
 	id JSONObj = [NSJSONSerialization JSONObjectWithData:[settingsValue dataUsingEncoding:NSUTF8StringEncoding]
                                                 options:NSJSONReadingAllowFragments
                                                   error:&error];
-	
+
 	if (error != nil) {
         NSLog(@"NSString JSONObject error: %@", [error localizedDescription]);
     }
 
     @try {
-	
+
 		if ([settingsType isEqual: @"string"] && [JSONObj isKindOfClass:[NSString class]]) {
 			[target setObject:(NSString*)JSONObj forKey:settingsName];
 		} else if ([settingsType  isEqual: @"number"] && [JSONObj isKindOfClass:[NSNumber class]]) {
@@ -135,21 +142,22 @@
 			// data
 			[target setObject:[settingsValue dataUsingEncoding:NSUTF8StringEncoding] forKey:settingsName];
 		}
-		
+
 		if (target != defaults)
 			[defaults setObject:(NSMutableDictionary*)target forKey:settingsDict];
 		[defaults synchronize];
 
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-		
+
     } @catch (NSException * e) {
-	
+
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT messageAsString:[e reason]];
-    
+
 	} @finally {
-	
+
         [self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
     }
+	}];
 }
 /*
   Parsing the Root.plist for the key, because there is a bug/feature in Settings.bundle
@@ -162,16 +170,16 @@
 	NSString *pathStr = [[NSBundle mainBundle] bundlePath];
 	NSString *settingsBundlePath = [pathStr stringByAppendingPathComponent:@"Settings.bundle"];
 	NSString *finalPath = [settingsBundlePath stringByAppendingPathComponent:@"Root.plist"];
-	
+
 	NSDictionary *settingsDict = [NSDictionary dictionaryWithContentsOfFile:finalPath];
 	NSArray *prefSpecifierArray = [settingsDict objectForKey:@"PreferenceSpecifiers"];
 	NSDictionary *prefItem;
 	for (prefItem in prefSpecifierArray)
 	{
-		if ([[prefItem objectForKey:@"Key"] isEqualToString:settingsName]) 
-			return [prefItem objectForKey:@"DefaultValue"];		
+		if ([[prefItem objectForKey:@"Key"] isEqualToString:settingsName])
+			return [prefItem objectForKey:@"DefaultValue"];
 	}
 	return nil;
-	
+
 }
 @end
