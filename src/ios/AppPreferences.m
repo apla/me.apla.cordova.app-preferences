fd0c6521965749e319c512a9e3b70b082a342b92
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2016-01-05 13:20:35 +0100

    Support for breaking change in cordova-ios 4 (self.webView is no UIWebView anymore, WKWebView support included)

--- src/ios/AppPreferences.m
+++ src/ios/AppPreferences.m
@@ -21,7 +21,12 @@
 - (void)defaultsChanged:(NSNotification *)notification {
 
 	NSString * jsCallBack = [NSString stringWithFormat:@"cordova.fireDocumentEvent('preferencesChanged');"];
+
+#ifdef __CORDOVA_4_0_0
+	[self.webViewEngine evaluateJavaScript:jsCallBack completionHandler:nil];
+#else
 	[self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+#endif
 }
 
 
