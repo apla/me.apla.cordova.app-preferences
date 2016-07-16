fc30fd9cb51e91d665b124901efb726bd4698c22
apla <dot.and.thing@gmail.com>
2016-07-17 01:45:19 +0300

    another try on settings flyout

--- src/windows8/platform.js
+++ src/windows8/platform.js
@@ -151,119 +151,122 @@ AppPreferencesW8.prototype.show = function (successCallback, errorCallback, args
 //  •It is called when the current app does not have focus.
 //  •It is called when the pane is already displayed.
 
-    try {
-        var settingsPane = Windows.UI.ApplicationSettings.SettingsPane.show();
-    } catch (e) {
+	try {
+		var settingsPane = Windows.UI.ApplicationSettings.SettingsPane.show();
+	} catch (e) {
 
-    }
+	}
 
-    successCallback();
+	successCallback();
 
-    return;
+	return;
 
-    // adding command to settings charm example
+	// adding command to settings charm example
 
-    var settingsPane = Windows.UI.ApplicationSettings.SettingsPane.getForCurrentView();
+	var settingsPane = Windows.UI.ApplicationSettings.SettingsPane.getForCurrentView();
 
-    function commandsRequested(eventArgs) {
+	function commandsRequested(eventArgs) {
 
-        var applicationCommands = eventArgs.request.applicationCommands;
+		var applicationCommands = eventArgs.request.applicationCommands;
 
-        var privacyCommand = new Windows.UI.ApplicationSettings.SettingsCommand('privacy', 'Privacy Policy', function () {
+		var privacyCommand = new Windows.UI.ApplicationSettings.SettingsCommand('privacy', 'Privacy Policy', function () {
 
-            window.open('index.html');
+			window.open('index.html');
 
-        });
+		});
 
-        applicationCommands.append(privacyCommand);
+		applicationCommands.append(privacyCommand);
 
-    }
+	}
 
-    settingsPane.addEventListener("commandsrequested", commandsRequested);
+	settingsPane.addEventListener("commandsrequested", commandsRequested);
 
-    return;
+	return;
 
-    // another way (not works)
-    "use strict";
-    var page = WinJS.UI.Pages.define("/html/4-ProgrammaticInvocation.html", {
-        ready: function (element, options) {
-            document.getElementById("scenario4Add").addEventListener("click", scenario4AddSettingsFlyout, false);
-            document.getElementById("scenario4Show").addEventListener("click", scenario4ShowSettingsFlyout, false);
+	// another way (not works)
+	"use strict";
+	var page = WinJS.UI.Pages.define("/html/4-ProgrammaticInvocation.html", {
+		ready: function (element, options) {
+			document.getElementById("scenario4Add").addEventListener("click", scenario4AddSettingsFlyout, false);
+			document.getElementById("scenario4Show").addEventListener("click", scenario4ShowSettingsFlyout, false);
 
-            // clear out the current on settings handler to ensure scenarios are atomic
-            WinJS.Application.onsettings = null;
+			// clear out the current on settings handler to ensure scenarios are atomic
+			WinJS.Application.onsettings = null;
 
-            // Display invocation instructions in the SDK sample output region
-            WinJS.log && WinJS.log("To show the settings charm, invoke the charm bar by swiping your finger on the right edge of the screen or bringing your mouse to the lower-right corner of the screen, then select Settings. Or you can just press Windows logo + i. To dismiss the settings charm, tap in the application, swipe a screen edge, right click, invoke another charm or application.", "sample", "status");
-        }
-    });
+			// Display invocation instructions in the SDK sample output region
+			WinJS.log && WinJS.log("To show the settings charm, invoke the charm bar by swiping your finger on the right edge of the screen or bringing your mouse to the lower-right corner of the screen, then select Settings. Or you can just press Windows logo + i. To dismiss the settings charm, tap in the application, swipe a screen edge, right click, invoke another charm or application.", "sample", "status");
+		}
+	});
 
-    function scenario4AddSettingsFlyout() {
-        WinJS.Application.onsettings = function (e) {
-            e.detail.applicationcommands = {
-                "defaults": {
-                    title: "Defaults",
-                    href: "/html/4-SettingsFlyout-Settings.html"
-                }
-            };
-            WinJS.UI.SettingsFlyout.populateSettings(e);
-        };
-        // Make sure the following is called after the DOM has initialized. Typically this would be part of app initialization
-        WinJS.Application.start();
+	function scenario4AddSettingsFlyout() {
+		WinJS.Application.onsettings = function (e) {
+			e.detail.applicationcommands = {
+				"defaults": {
+					title: "Defaults",
+					href: "/html/4-SettingsFlyout-Settings.html"
+				}
+			};
+			WinJS.UI.SettingsFlyout.populateSettings(e);
+		};
+		// Make sure the following is called after the DOM has initialized. Typically this would be part of app initialization
+		WinJS.Application.start();
 
-        // Display a status message in the SDK sample output region
-        WinJS.log && WinJS.log("Defaults settings flyout added from 4-SettingsFlyout-Settings.html", "samples", "status");
-    }
+		// Display a status message in the SDK sample output region
+		WinJS.log && WinJS.log("Defaults settings flyout added from 4-SettingsFlyout-Settings.html", "samples", "status");
+	}
 
-    function scenario4ShowSettingsFlyout() {
-        WinJS.UI.SettingsFlyout.showSettings("defaults", "/html/4-SettingsFlyout-Settings.html");
+	function scenario4ShowSettingsFlyout() {
+		WinJS.UI.SettingsFlyout.showSettings("defaults", "/html/4-SettingsFlyout-Settings.html");
 
-        // Display a status message in the SDK sample output region
-        WinJS.log && WinJS.log("Defaults settings flyout showing", "samples", "status");
-    }
+		// Display a status message in the SDK sample output region
+		WinJS.log && WinJS.log("Defaults settings flyout showing", "samples", "status");
+	}
 };
 
 
 if (!window.WinJS.UI.SettingsFlyout) {
-    var scriptElem = document.createElement("script");
+	var scriptElem = document.createElement("script");
 
-    if (navigator.appVersion.indexOf("Windows Phone 8.1;") !== -1) {
-        // not supported: https://msdn.microsoft.com/en-us/library/windows/apps/hh701253.aspx
+	if (navigator.appVersion.indexOf('MSAppHost/3.0') !== -1) {
+		// Windows 10 UWP
+		scriptElem.src = '/WinJS/js/ui.js';
+	} else if (navigator.appVersion.indexOf("Windows Phone 8.1;") !== -1) {
+		// not supported: https://msdn.microsoft.com/en-us/library/windows/apps/hh701253.aspx
 		// windows phone 8.1 + Mobile IE 11
-        // scriptElem.src = "//Microsoft.Phone.WinJS.2.1/js/ui.js";
-    } else if (navigator.appVersion.indexOf("MSAppHost/2.0;") !== -1) {
-        // windows 8.1 + IE 11
-        scriptElem.src = "//Microsoft.WinJS.2.0/js/ui.js";
-    } else {
-        // windows 8.0 + IE 10
-        scriptElem.src = "//Microsoft.WinJS.1.0/js/ui.js";
-    }
-    scriptElem.addEventListener("load", onWinFlyoutReady);
-    document.head.appendChild(scriptElem);
+		// scriptElem.src = "//Microsoft.Phone.WinJS.2.1/js/ui.js";
+	} else if (navigator.appVersion.indexOf("MSAppHost/2.0;") !== -1) {
+		// windows 8.1 + IE 11
+		scriptElem.src = "//Microsoft.WinJS.2.0/js/ui.js";
+	} else {
+		// windows 8.0 + IE 10
+		scriptElem.src = "//Microsoft.WinJS.1.0/js/ui.js";
+	}
+	scriptElem.addEventListener("load", onWinFlyoutReady);
+	document.head.appendChild(scriptElem);
 }
 else {
-    onWinFlyoutReady();
+	onWinFlyoutReady();
 }
 
 function onWinFlyoutReady() {
-    AddSettingsFlyout();
+	AddSettingsFlyout();
 }
 
 function AddSettingsFlyout() {
-    WinJS.Application.onsettings = function (e) {
-        e.detail.applicationcommands = {
-            "Preferences": {
-                title: "Preferences",
-                href: "/www/apppreferences.html"
-            }
-        };
-        WinJS.UI.SettingsFlyout.populateSettings(e);
-    };
-    // Make sure the following is called after the DOM has initialized. Typically this would be part of app initialization
-    WinJS.Application.start();
+	WinJS.Application.onsettings = function (e) {
+		e.detail.applicationcommands = {
+			"Preferences": {
+				title: "Preferences",
+				href: "/www/apppreferences.html"
+			}
+		};
+		WinJS.UI.SettingsFlyout.populateSettings(e);
+	};
+	// Make sure the following is called after the DOM has initialized. Typically this would be part of app initialization
+	WinJS.Application.start();
 
-    // Display a status message in the SDK sample output region
-    WinJS.log && WinJS.log("Defaults settings flyout added from 4-SettingsFlyout-Settings.html", "samples", "status");
+	// Display a status message in the SDK sample output region
+	WinJS.log && WinJS.log("Defaults settings flyout added from 4-SettingsFlyout-Settings.html", "samples", "status");
 }
 
 module.exports = new AppPreferencesW8();
